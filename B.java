6f889c95fb6eba12c86e19382296b2a64f7fdbd1
IgorFreiredeMorais <75400939+IgorFreiredeMorais@users.noreply.github.com>
2022-02-05 19:02:06 -0300

    Add files via upload

---
+++ B.java
@@ -0,0 +1,35 @@
+public class B {
+
+  private int B1;
+  private float B2;
+
+  public A(int B1, float B2) {
+    this.B1 = B1;
+    this.B2 = B2;
+  }
+
+  public void MB1(){
+    System.out.println(“Nome do método: MB1”);
+  }
+  
+   public void MB2(){
+    System.out.println(“Nome do método: MB2”);
+  }
+
+  public int getB1() {
+    return B1;
+  }
+
+  public void setB1(int B1) {
+    this.B1 = B1;
+  }
+
+  public float getB2() {
+    return salario;
+  }
+
+  public void setB2(float B2) {
+    this.B2 = B2;
+  }
+
+}
